da1681e50de1c3a679be71f1b1437f34db26bb39
IsaacFeld <isaacbfeldman@gmail.com>
2025-05-06 18:26:14 +0200

    v1.0.0 Electron Forge Commit

--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron')
 const fs = require('fs-extra')
+const path = require('path')
 let fileMapIndex = new Map()
 
 contextBridge.exposeInMainWorld('electronAPI', {
@@ -13,26 +14,26 @@ contextBridge.exposeInMainWorld('electronAPI', {
     saveFile : (fileElement) => saveFile(fileElement)
 })
 function addFile(currentPath, fileId){
-  fs.writeFileSync(`save/${currentPath}${fileId}.md`, '')
+  fs.writeFileSync(path.join(__dirname, `save/${currentPath}${fileId}.md`), '')
 }   
 function addFolder(currentPath, folderId){
-  fs.mkdirSync(`save/${currentPath}${folderId}`)
+  fs.mkdirSync(path.join(__dirname, `save/${currentPath}${folderId}`))
 }
 function removeFile(currentPath, fileId){
-  fs.rmSync(`save/${currentPath}${fileId}.md`)
+  fs.rmSync(path.join(__dirname, `save/${currentPath}${fileId}.md`))
 }
 function removeFolder(currentPath, folderId){
-  fs.rmSync(`save/${currentPath}${folderId}`, {recursive: true, force: true })
+  fs.rmSync(path.join(__dirname, `save/${currentPath}${folderId}`), {recursive: true, force: true })
 }
 function move(currentPath, fileId, targetId, isFolder){
   if(targetId != undefined){ // Not a return dir case
     lastPath = currentPath + targetId + '/'
     if(isFolder){
-      fs.moveSync(`save/${currentPath}${fileId}`, `save/${currentPath}${targetId}/${fileId}`) // markdown string
+      fs.moveSync(path.join(__dirname, `save/${currentPath}${fileId}` ), path.join(__dirname,`save/${currentPath}${targetId}/${fileId}`)) // markdown string
 
     }
     else{
-      fs.moveSync(`save/${currentPath}${fileId}.md`, `save/${currentPath}${targetId}/${fileId}.md`) // markdown string
+      fs.moveSync(path.join(__dirname, `save/${currentPath}${fileId}.md`), path.join(__dirname, `save/${currentPath}${targetId}/${fileId}.md`)) // markdown string
     }
 
   }
@@ -41,17 +42,17 @@ function move(currentPath, fileId, targetId, isFolder){
     returnPath.splice(returnPath.length - 2, 1)
     returnPath = returnPath.join('/');
     if(isFolder){
-      fs.moveSync(`save/${currentPath}${fileId}`, `save/${returnPath}${fileId}`) // markdown string
+      fs.moveSync(path.join(__dirname, `save/${currentPath}${fileId}`), path.join(__dirname, `save/${returnPath}${fileId}`)) // markdown string
     }
     else{
-      fs.moveSync(`save/${currentPath}${fileId}.md`, `save/${returnPath}${fileId}.md`) // markdown string
+      fs.moveSync(path.join(__dirname, `save/${currentPath}${fileId}.md`), path.join(__dirname, `save/${returnPath}${fileId}.md`)) // markdown string
     }
 
 
   }
 }
 function getFilemap(){
-  return fs.readFileSync('save/filemap.json', 'utf-8')
+  return fs.readFileSync(path.join(__dirname, 'save/filemap.json'), 'utf-8')
 }
 function setFilemap(fileMap){ // initial setting of indexes ( this needs to be called multiple times )
   let i = 0; // maybe more efficient way?  Could put this into front end, and use the length to update indexes of newly added files?
@@ -60,21 +61,21 @@ function setFilemap(fileMap){ // initial setting of indexes ( this needs to be c
     i++
   });
   if(fileMap){
-    fs.writeFileSync('save/filemap.json', JSON.stringify([...fileMap], null, 2))
+    fs.writeFileSync(path.join(__dirname, 'save/filemap.json'), JSON.stringify([...fileMap], null, 2))
   }
 }
 function saveFile(fileElement){
-  fileMap = JSON.parse(fs.readFileSync('save/filemap.json', 'utf-8')) // overwrites filemap.json array using position based on set filemap function
+  fileMap = JSON.parse(fs.readFileSync(path.join(__dirname, 'save/filemap.json'), 'utf-8')) // overwrites filemap.json array using position based on set filemap function
   if(fileElement){ // If it's a delete event on a new file, file element will be null
     fileMap[fileMapIndex.get(fileElement.id)][0] = fileElement.id 
     fileMap[fileMapIndex.get(fileElement.id)][1] = fileElement
     if(fileElement.md && fileElement.md.length != 0){
-      fs.writeFile(`save/${fileElement.path}${fileElement.id}.md`, fileElement.md.join('\n'))
+      fs.writeFile(path.join(__dirname, `save/${fileElement.path}${fileElement.id}.md`), fileElement.md.join('\n'))
     } 
   }
 
 
-  fs.writeFileSync('save/filemap.json', JSON.stringify([...fileMap],null, 2 ))
+  fs.writeFileSync(path.join(__dirname, 'save/filemap.json'), JSON.stringify([...fileMap],null, 2 ))
 }
 
 /*
